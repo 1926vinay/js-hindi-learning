1395d8e41d5c670b9b9333b4920872270410cf53
1926vinay <100567545+1926vinay@users.noreply.github.com>
2024-03-08 18:26:58 +0000

    .

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,17 @@
 const accountId = 144553
 let accounEmail = "1926kumarvinay@gmail.com"
 var accountPassword = "1234"
-accountCity = "Patna"
+accountCity = "Patna" // also a way to declare the variable, but not a good practice.
+let accountState; // it will give the value undefined, until you assign a value to it.
+
+// accountId=2 // can't declare constant variables twice in the code.
+/*
+prefer not to use var 
+bacause of issues in block scope({}), and functional scope
+*/
+
+accountEmail="hc@gmail.com"
+accountPassword="2121"
+accountCity="rajasthan"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]); // it will give the result in tabular format. Just type variable names in square brackets, ex-> [variable_name1,variabl_name2...]

c6860f78b04f96f3547df7a332924feceeae9a86
1926vinay <100567545+1926vinay@users.noreply.github.com>
2024-03-11 07:14:29 +0000

    conversion and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,5 +25,29 @@ if isLoggedIn="vinay", booleanIsLoggedIn=true.
 
 let n=33;
 let abc=String(n);  //for conversion in String
-console.log(typeof abc); 
-console.log(abc);
+// console.log(typeof abc); 
+// console.log(abc);
+
+// **************************Operations****************************************
+let value=33;
+let negvalue=-value;
+//console.log(negvalue);
+
+// console.log("1"+2+2); //output=122
+// console.log(1+2+"2"); //output=32  // jo bhi datatype phle hga usi ke hisaab se aage wale ko bhi samjhega.
+
+console.log(true); //output=1;
+console.log(true+); //error;
+console.log(+""); //output=0;
+
+let count=100;
+count++;
+console.log(count); //output=101;
+
+/*
+study more about prefix and postfix from the below link.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+for type conversion below link.
+https://tc39.es/ecma262/multipage/abstract-operations.html
+*/

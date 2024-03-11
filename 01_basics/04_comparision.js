03e2d7c6d825d8a38bc0cc39ee05d14b6d6fd6cc
1926vinay <100567545+1926vinay@users.noreply.github.com>
2024-03-11 07:34:34 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+/*simple comparision, same datatypes.
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2>1);
+*/
+
+// console.log("2">1); //two different data types.
+// console.log(2>"1";);
+
+/*
+should be avoided(null,undefined), just for the knowledge.
+// console.log(null>0); //false
+// console.log(null=0);  //false
+// console.log(null>=0);  //true
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+*/
+
+// === (strict comparision, it checks for the data types also while comparing)
+console.log("2"===2); //output=false

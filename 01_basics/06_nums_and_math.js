9f042fbb04cabc1fc40875fddffc7aee53e9564e
1926vinay <100567545+1926vinay@users.noreply.github.com>
2024-03-12 17:15:48 +0000

    nums and maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -2,6 +2,8 @@ const score=400;
 const balance=new Number(100); //if we want data type to be number only.
 //console.log(balance);
 
+
+//.toFixed()  //how many decimal places you need to print.
 const balance2=balance.toString();
 //console.log(balance2.length);
 //console.log(balance.toFixed(2));//it will show upto 2 decimal places. ex->100.00
@@ -11,6 +13,8 @@ const balance2=balance.toString();
 const otherNumber=123.8966;
 //console.log(otherNumber.toPrecision(3)); //output-> 123.9   and if toPrecision(3),then value will be 124
 
+
+//.toLocaleString()  //i gives the given value in number system having commas in between them.
 const hundred=1000000;
 //console.log(hundred.toLocaleString());//it will give the according to US number system like->1,000,000;
 //console.log(hundred.toLocaleString('en-IN'));// now it will give according to Indian number system like-> 10,00,000
@@ -20,8 +24,8 @@ const hundred=1000000;
 // console.log(Math.round(4.3))//it represents according to basic maths.
 // console.log(Math.min(2,5,3,7,8)); // 3
 // console.log(Math.max(2,5,3,7,8)); // 8
-//console.log(Math.floor(3.0)); //it gives the nearest lowest integer. 3.1,3.2....3.9 for all these the output will be 3.
-//console.log(Math.ceil(3.0));  //it gives the nearest largets integer. 3.1,3.2....3.9 for all these the output will be 4.
+console.log(Math.floor(3.0)); //it gives the nearest lowest integer. 3.1,3.2....3.9 for all these the output will be 3.
+console.log(Math.ceil(3.0));  //it gives the nearest largets integer. 3.1,3.2....3.9 for all these the output will be 4.
 
 
 //Math.random() //it gives the value between 0 an 1 both inclusive.
@@ -33,4 +37,4 @@ const hundred=1000000;
 // if we want the values to be in a specific range then we can do the below process.
 const min=10;
 const max=20;
-console.log(Math.floor(Math.random()*(max-min+1))+min);
+//console.log(Math.floor(Math.random()*(max-min+1))+min);

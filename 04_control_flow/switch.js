1327316898f51f5ccda16af6954d4c4bbedbcfec
1926vinay <100567545+1926vinay@users.noreply.github.com>
2024-03-29 15:13:20 +0000

    control flow switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch statement //normal as we know
+
+//syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=2;  //"feb"  //in case of string checking
+switch (month){
+    case 1:  //case "janaury" and so on for string checking
+        console.log("January")
+        break;
+        case 2:
+            console.log("Feb")
+            break;
+            case3:
+            console.log("March")
+            break;
+            case 4:
+                console.log("April")
+                break;
+                default:
+                    console.log("default case matched")
+                    break;
+}
